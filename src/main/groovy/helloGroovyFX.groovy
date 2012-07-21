6c705c8607321652f3971d773282eeb672be3a14
Bill Pfeiffer <bill.pfeiffer@gmail.com>
2012-07-20 23:12:45 -0400

    Creating play-gh repo

---
+++ src/main/groovy/helloGroovyFX.groovy
@@ -0,0 +1,23 @@
+import groovyx.javafx.GroovyFX
+import groovyx.javafx.SceneGraphBuilder
+
+//
+// Program starts here - This app is super awesome
+//
+GroovyFX.start {
+    def sg = new SceneGraphBuilder()
+
+    sg.stage(title: "GroovyFX Hello World", visible: true) {
+        scene(fill: black, width: 650, height: 450) {
+            hbox(padding: 80) {
+                text(text: "Groovy", style: "-fx-font-size: 80pt") {
+                    fill linearGradient(endX: 0, stops: [palegreen, seagreen])
+                }
+                text(text: "FX", style: "-fx-font-size: 80pt") {
+                    fill linearGradient(endX: 0, stops: [cyan, dodgerblue])
+                    effect dropShadow(color: dodgerblue, radius: 25, spread: 0.25)
+                }
+            }
+        }
+    }
+}
